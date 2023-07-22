54677e399b7daa27902300f0338816f04efe90ef
Gregor Purdy <gregor@abcelo.com>
2023-07-22 05:40:35 -0700

    Use TestRandom for real in bench

--- bench/src/main/scala/com/gregorpurdy/ccs/Modulus10DoubleAddDoubleBench.scala
+++ bench/src/main/scala/com/gregorpurdy/ccs/Modulus10DoubleAddDoubleBench.scala
@@ -1,21 +1,22 @@
 package com.gregorpurdy.ccs
 
 import org.openjdk.jmh.annotations.*
+import zio.Runtime
+import zio.Unsafe
 import zio.test.*
-import zio.{Runtime, Unsafe}
-// import zio.ZLayer
 
 @State(Scope.Benchmark)
 @BenchmarkMode(Array(Mode.AverageTime))
 class Modulus10DoubleAddDoubleBench {
   val iterations = 10000
   val size = 1000000
-  val effect = Gen.alphaNumericStringBounded(8, 8).sample.take(size).map(_.value.toUpperCase).runCollect
+  val effect = Gen.alphaNumericStringBounded(8, 8).runCollectN(size).map(_.map(_.toUpperCase))
+
   val cases = Unsafe
     .unsafe { implicit unsafe =>
       zio.Runtime.default.unsafe
         .run(
-          effect // .provide(ZLayer.succeed(TestRandom))
+          effect.provideLayer(TestRandom.deterministic)
         )
         .getOrThrowFiberFailure()
     }
